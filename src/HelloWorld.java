d52835e5af8975693f961f7e633ff81b84085c71
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-20 16:36:22 -0500

    This is my third commit.

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("I created first Java project and pushed to GitHub");
+        System.out.print("Welcome to SDET Course :  ");
+        System.out.println("This line is starting with the previous line.");
+        System.out.println("Again We starting with a new line.");
+
+        /**
+        When you use the just print command at the and of "System.out.print" ,
+
+         current line and next line will print in the same line.
+         For example check line 6 and 7.
+         */
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I created first Java project and pushed to GitHub");
-        System.out.println("Hi Guys!!!");
+
     }
 }
