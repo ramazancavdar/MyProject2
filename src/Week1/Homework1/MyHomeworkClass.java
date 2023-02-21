2142b142c00175cdc9b283c360cc16e90a756dd0
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-20 20:04:28 -0500

    I created Student class and corrected comments.

---
+++ src/Week1/Homework1/MyHomeworkClass.java
@@ -0,0 +1,31 @@
+package Week1.Homework1;
+//Java Homeworks: PART 1
+//
+//1-) Follow the below steps :
+//	a-) Create a class name called : MyHomeworkClass
+//	b-) Create a main metohd in MyHomeworkClass
+//	c-) Print the following text in CONSOLE
+//		"Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible."
+//
+//2-) Write below sentences in comments
+//	a-) This is single line comment
+//	b-) This is regular multi-line comment
+//	c-) This is Java Doc comment and it appears in green color.
+//
+//3-) Write the below sentences in single line, multi-line and JavaDoc format
+//		Class Name: MyHomeworkClass
+//		Method Name: main()
+//		Developer: <Write your Name>
+//		Date: November 23,2022
+//
+public class MyHomeworkClass {
+    public static void main(String[] args) {
+        System.out.println("Java is a high-level, class-based, " +
+                "object-oriented programming language that is designed to " +
+                "have as few implementation dependencies as possible.");
+
+        //  a-) This is single line comment
+        //	b-) This is regular multi-line comment
+        //	c-) This is Java Doc comment and it appears in green color.
+    }
+}
