e6c75c3c884d456c319bcf6c418d65f6d9532788
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-20 19:00:39 -0500

    I added PrimitiveDataTypes class.

--- src/Week1/DataTypes.java
+++ src/Week1/DataTypes.java
@@ -17,11 +17,14 @@ public class DataTypes  {
 
         int age2 = 11, height1 = 120, distance1 = 100;
 
-        System.out.println(age1);
+        System.out.println("age's value : " + age);
         System.out.println(height1);
         System.out.println(distance1);
 
         System.out.println(initial);
+
+        age = 50;
+        System.out.println("New age's value : " + age);
 
     }
 }
---
+++ src/Week1/PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+package Week1;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        boolean isCold = true;
+        boolean isHeavy = false;
+
+        char initial = 'C';
+
+        byte age = 22;
+
+        short salary = 1000;
+
+        System.out.println(isCold);
+        System.out.println(isHeavy);
+        System.out.println(initial);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
